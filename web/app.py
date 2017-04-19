27f905a1be4dd82095a67ff8f0d16f1c4ef4c2f9
root <root@ip-172-31-25-92.eu-central-1.compute.internal>
2017-04-19 15:57:55 +0000

    s3 storage

--- web/app.py
+++ web/app.py
@@ -1,3 +1,8 @@
+import boto3
+
+from botocore.client import Config
+from media.s3_storage import S3MediaStorage
+
 from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for
@@ -10,7 +15,10 @@ def upload():
 @app.route("/upload", methods=["POST"])
 def upload_file():
     uploaded_file = request.files['uploaded_media']
-    uploaded_file.save('/home/ec2-user/media-upload/var/files/' + uploaded_file.filename)
+    #uploaded_file.save('/home/ec2-user/media-upload/var/files/' + uploaded_file.filename)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    media_storage = S3MediaStorage(s3=s3, bucket_name='pkozak')
+    media_storage.store(key=uploaded_file.filename, media=uploaded_file)
     return redirect(url_for('upload'))
 
 if __name__ == "__main__":
