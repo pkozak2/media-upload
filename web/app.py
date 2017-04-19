152b9c8533761ab81355db776b087ea12248bcf3
EC2 Default User <ec2-user@ip-172-31-25-92.eu-central-1.compute.internal>
2017-04-19 15:36:52 +0000

    savefile

--- web/app.py
+++ web/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def upload():
+    return render_template('upload_file.html')
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    uploaded_file = request.files['uploaded_media']
+    uploaded_file.save('/home/ec2-user/media-upload/var/files/' + uploaded_file.filename)
+    return redirect(url_for('upload'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=80, debug=True)
